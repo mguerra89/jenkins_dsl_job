c9de199b6fba77b7ae93ba501d181869e0f3c9fd
mguerra89 <71152467+mguerra89@users.noreply.github.com>
2022-01-18 17:17:22 -0500

    Create parametrizadoDSL.groovy

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,25 @@
+job('Ejemplo-job1-DSL'){
+  description('Job DSL descripcion')
+  scm {
+    git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      node / gitConfigName('macloujulian')
+      node / gitConfigEmail('macloujulian@gmail.com')
+    }
+  }
+  parameters {
+    stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
+  
+  triggers {
+    cron('H/7 * * * *')
+  }
+  steps {
+    shell("bash jobscript.sh")
+  }
+   publishers {
+    mailer('mguerran@redsis.com', true, true)
+   }
+  
+}
